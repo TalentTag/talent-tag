758b46235db12bc1c13694656fd22d49ea088402
artshpakov <artshpakov@yandex.ru>
2014-03-22 14:20:10 +0400

    Omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :developer unless Rails.env.production?
+  config = YAML.load_file("#{Rails.root}/config/omniauth.yml")[Rails.env].symbolize_keys
 
-  provider :facebook, "176776305712139", "ddc868dc01d6ce0a6504d8f226e1ec0c"
-  provider :twitter, "5E7GchtEUZDfSi4Azz87w", "N9onKDLHbvKTNb0m4nICF1pYr3g0KX6pl89kphbo1M"
-  provider :vkontakte, "4201311", "5qFEBPOnhd4aFAOCS4Eh"
-  provider :google_oauth2, "428047194229-mgif4uclcvujg0bma1d2lr2i57lp80s4.apps.googleusercontent.com", "NOQvqMpHpfI1eZpkJfVWJUlM"
+  config.each do |provider, params|
+    provider provider, params['app_id'], params['app_secret']
+  end
+
+  TalentTag::Application::PROVIDERS = config
 end
