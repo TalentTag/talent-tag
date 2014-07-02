0e956498f52be3e85d6f7b0ce3bf6ee3d0053d1f
artshpakov <artshpakov@yandex.ru>
2014-07-02 12:39:42 +0400

    Fixed message text max length

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140701184309) do
+ActiveRecord::Schema.define(version: 20140702083555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 20140701184309) do
   create_table "messages", force: true do |t|
     t.integer  "conversation_id"
     t.integer  "user_id",         null: false
-    t.string   "text",            null: false
+    t.text     "text",            null: false
     t.datetime "created_at"
   end
 
