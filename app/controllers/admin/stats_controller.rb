ba4c445e931f8163aca131795b579360fa63e470
Artem Shpakov <artshpakov@yandex.ru>
2014-07-15 00:44:18 +0400

    Total entries count

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -3,8 +3,10 @@ class Admin::StatsController < Admin::BaseController
   def entries
     @sources = Source.visible.sort_by { |s| s.name }
     sql = if @current_source = @sources.find { |s| s.id == params[:source_id].to_i }
+      @total_count = Entry.where(source_id: params[:source_id].to_i).count
       "SELECT COUNT(*), EXTRACT(MONTH FROM created_at) AS month, DATE(created_at) AS date FROM entries WHERE EXTRACT(YEAR FROM created_at)=#{ params[:year] } AND source_id=#{ params[:source_id].to_i } GROUP BY month, date ORDER BY date DESC"
     else
+      @total_count = Entry.count
       "SELECT COUNT(*), EXTRACT(MONTH FROM created_at) AS month, DATE(created_at) AS date FROM entries WHERE EXTRACT(YEAR FROM created_at)=#{ params[:year] } GROUP BY month, date ORDER BY date DESC"
     end
     @entries_counters = ActiveRecord::Base.connection.execute sql
