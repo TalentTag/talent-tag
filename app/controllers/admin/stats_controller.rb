9aaf99901c29bcd3004b02748f9cfeecbc4b9135
Artem Shpakov <artshpakov@yandex.ru>
2014-07-15 16:34:10 +0400

    A few admin panel fixes

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -4,10 +4,10 @@ class Admin::StatsController < Admin::BaseController
     @sources          = Source.visible.sort_by { |s| s.name }
 
     sql = if @current_source = @sources.find { |s| s.id == params[:source_id].to_i }
-      @total_count = Rails.cache.fetch("stats:entries_total_count", expires_in: 10.minutes) { Entry.where(source_id: params[:source_id].to_i).count }
+      @total_count = Entry.where(source_id: params[:source_id].to_i).count
       "SELECT COUNT(*), EXTRACT(MONTH FROM created_at) AS month, DATE(created_at) AS date FROM entries WHERE EXTRACT(YEAR FROM created_at)=#{ params[:year] } AND source_id=#{ params[:source_id].to_i } GROUP BY month, date ORDER BY date DESC"
     else
-      @total_count = Rails.cache.fetch("stats:entries_total_count", expires_in: 10.minutes) { Entry.count }
+      @total_count = Entry.count
       "SELECT COUNT(*), EXTRACT(MONTH FROM created_at) AS month, DATE(created_at) AS date FROM entries WHERE EXTRACT(YEAR FROM created_at)=#{ params[:year] } GROUP BY month, date ORDER BY date DESC"
     end
 
