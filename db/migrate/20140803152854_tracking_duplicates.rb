97ca3c985bc634fcc71f0bec440a41084e30fc8b
Artem Shpakov <artshpakov@yandex.ru>
2014-08-03 22:39:29 +0400

    Fixing duplicates-related migration

--- db/migrate/20140803152854_tracking_duplicates.rb
+++ db/migrate/20140803152854_tracking_duplicates.rb
@@ -2,11 +2,6 @@ class TrackingDuplicates < ActiveRecord::Migration
 
   def change
     add_column :entries, :duplicate_of, :integer
-
-    Entry.all.each do |entry|
-      original = Entry.where("body='#{entry.body}' AND id!=#{entry.id}").order(:created_at).limit(1).try :first
-      entry.update duplicate_of: original.id if original and original.duplicate_of.nil?
-    end
   end
 
 end
