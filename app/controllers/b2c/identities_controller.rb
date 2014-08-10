4e0e7e7f2cd851787bd044bc39158763a0fccb2e
Artem Shpakov <artshpakov@yandex.ru>
2014-08-10 14:45:56 +0400

    Fixed image source fetching

--- app/controllers/b2c/identities_controller.rb
+++ app/controllers/b2c/identities_controller.rb
@@ -10,7 +10,6 @@ class B2c::IdentitiesController < B2c::BaseController
       render nothing: true, status: :created
     else
       return render json: { errors: { 'user.email' => "Этот адрес уже зарегистрирован в системе. Если пароль от аккаутна утерян, воспользуйтесь опцией восстановления пароля." } }, status: 422 if User.exists?(email: params[:identity][:user_attributes][:email])
-      respond_with identity
     end
   end
 
@@ -18,7 +17,7 @@ class B2c::IdentitiesController < B2c::BaseController
   protected
 
   def create_params
-    profile_params = %w(location image nickname birthdate education work url_facebook url_twitter url_vkontakte url_google_oauth2)
+    profile_params = %w(location image image_source nickname birthdate education work url_facebook url_twitter url_vkontakte url_google_oauth2)
     cparams = params.require(:identity).permit(:provider, :uid, user_attributes: [:email, :firstname, :lastname, profile: profile_params])
     password = Digest::MD5.hexdigest(Time.now.to_s + cparams[:uid] + cparams[:provider])
     cparams[:user_attributes][:password] = cparams[:user_attributes][:password_confirmation] = password
