1c0503b6ed1612ec8bff86ddc0fed4edf7a54d56
Artem Shpakov <artshpakov@yandex.ru>
2014-08-27 00:02:53 +0400

    Fixed B2C-account template name

--- app/controllers/b2c/account_controller.rb
+++ app/controllers/b2c/account_controller.rb
@@ -2,7 +2,7 @@ class B2c::AccountController < B2c::BaseController
 
   def show
     gon.user = @user = User.find_by! id: params[:id]
-    render "account/b2c"
+    render "b2c/account"
   end
 
 end
