69df592a7515ead50b18297359fa3bea86cbd0d9
Artem Shpakov <artshpakov@yandex.ru>
2014-09-03 12:16:13 +0400

    Notifications individual helpers

--- app/helpers/b2b_helper.rb
+++ app/helpers/b2b_helper.rb
@@ -7,4 +7,10 @@ module B2bHelper
     end
   end
 
+  %w(status_change new_post).each do |event|
+    define_method("notification_#{ event }") do |notification|
+      render partial: "b2b/partials/notifications/#{ event }", locals: { notification: notification }
+    end
+  end
+
 end
