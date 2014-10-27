5040c1a6eae2944b7cf521aa31bcb8118e59fbe2
Artem Shpakov <artshpakov@yandex.ru>
2014-10-28 00:10:24 +0400

    Adding http prefix to portfolio hrefs in there's none

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -14,6 +14,7 @@ class PortfolioController < ApplicationController
   protected
 
   def create_params
+    params[:portfolio].update href: "http://#{ params[:portfolio][:href] }" unless params[:portfolio][:href].include? 'http://'
     params.require(:portfolio).permit %i(title href)
   end
 
