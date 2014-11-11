e8ae49d5f6896e24388cd66aa67cf21051d41a93
Artem Shpakov <artshpakov@yandex.ru>
2014-11-11 09:52:32 +0300

    Features page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ TalentTag::Application.routes.draw do
 
   root to: "account#index", as: :account
   scope controller: :public do
-    get :promo, :about, :contacts, :faq
+    get :promo, :about, :contacts, :faq, :features
   end
 
 
