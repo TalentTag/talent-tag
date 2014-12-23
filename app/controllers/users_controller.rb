bc4b45e09d81a597de913cc986535ec71e51dc6b
Artem Shpakov <artshpakov@yandex.ru>
2014-12-23 10:14:10 +0300

    B2c signup fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
 
   def create
-    @user = User.new create_params
+    @user = Specialist.new create_params
     if @user.save
       sign_user_in @user, as: params[:type]
       respond_with @user, status: :created
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   end
 
   def search
-    @users = User.search Array.wrap(params[:tags].split(',')).map { |tag| "(#{ tag })" }.join(" | ")
+    @users = Specialist.search Array.wrap(params[:tags].split(',')).map { |tag| "(#{ tag })" }.join(" | ")
     render 'users/index'
   end
 
