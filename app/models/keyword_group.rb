b778c5621aba34ca6ba42beba7e4a226562be75d
Nick Chernyshev <nick.chernyshev@gmail.com>
2015-03-26 22:03:14 +0500

    refactor keyword group

--- app/models/keyword_group.rb
+++ app/models/keyword_group.rb
@@ -4,20 +4,18 @@ class KeywordGroup < ActiveRecord::Base
   belongs_to :area
 
   scope :overlapped, ->(kw_array) { where.overlap(keywords: kw_array) }
-  scope :keywords, -> { pluck(:keywords).flatten }
-  scope :exceptions, -> { pluck(:exceptions).flatten }
-
 
   class << self
+    def get_array(field)
+      pluck(field.to_sym).flatten.map{|el| el unless el.blank? }.compact
+    end
+
     def join_keywords(keyword_groups)
-      keyword_groups.keywords.map do |kw|
-        kw.strip!
-        "\"#{kw}\""
-      end.join(' | ')
+      keyword_groups.get_array(:keywords).map { |kw| "\"#{kw}\"" }.join(' | ')
     end
 
     def join_exceptions(keyword_groups)
-      keyword_groups.exceptions.map { |ex| "!\"#{ex}\"" }.join(' ')
+      keyword_groups.get_array(:exceptions).map { |ex| "!\"#{ex}\"" }.join(' ')
     end
 
     def keyword_query(keyword_groups)
