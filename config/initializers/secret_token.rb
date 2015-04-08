521fc80d46d536aa0602e55c20311f61b04dd85b
Nick Chernyshev <nick.chernyshev@gmail.com>
2015-04-08 23:40:32 +0500

    fix stage sessions

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TalentTag::Application.config.secret_key_base = 'f0754499ffc5f7c061758e35d66ee500c55fe77c10e719e6a2b3886c73033d42f765160e2631320cb223f04c74517d20c8ed142a10399c01b6d8704b55372c12'
+TalentTag::Application.config.secret_key_base = ENV["secret_key_base"]
