61a02ac420ce532793f1a06f8858bc1fabdc4545
Artem Shpakov <artshpakov@yandex.ru>
2015-04-28 17:03:23 +0300

    New entries in search counter

--- app/models/search.rb
+++ app/models/search.rb
@@ -27,4 +27,13 @@ class Search < ActiveRecord::Base
     touch && save
   end
 
+  def new_entries_count
+    @count ||= Entry.search_for_ids(query, without: { source_id: Source.unpublished }, with: { fetched_at: last_checked_at..Time.now }).total_count
+  end
+
+
+  def as_json params
+    super(params).merge 'new_entries_count'=>new_entries_count
+  end
+
 end
--- app/workers/mail_updates_worker.rb
+++ app/workers/mail_updates_worker.rb
@@ -6,8 +6,7 @@ class MailUpdatesWorker
     searches = Search.where(user_id: user['id'])
     updates = {}
     searches.each do |search|
-      count = Entry.search_for_ids(search.query, without: { source_id: Source.unpublished }, with: { fetched_at: search.last_checked_at..Time.now }).total_count
-      updates[search.name] = count unless count.zero?
+      updates[search.name] = new_entries_count unless new_entries_count.zero?
     end
     UpdatesMailer.new_entries(user['email'], updates).deliver if updates.present?
   end
