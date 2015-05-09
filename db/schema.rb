11699d9392768be2ad6460449a7dc561235831e9
Nick Chernyshev <nick.chernyshev@gmail.com>
2015-05-10 00:46:32 +0500

    rebase development

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428103854) do
+ActiveRecord::Schema.define(version: 20150504152220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+  enable_extension "pg_trgm"
+  enable_extension "btree_gist"
 
   create_table "areas", force: true do |t|
     t.string "name"
