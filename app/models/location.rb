f5488ef98b43c0530018435f4800d96b4cc7b484
artshpakov <artshpakov@yandex.ru>
2015-06-29 18:44:38 +0300

    Ordering locations by ids

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,8 @@ class Location < ActiveRecord::Base
 
   scope :name_like, ->(term) { where{ name.matches "%#{ term.neat.downcase }%" } }
 
+  default_scope -> { order :id }
+
   def self.first_by_name(term)
     where(name: term).pluck(:id).first
   end
