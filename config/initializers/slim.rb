656332768d4a29df884db866c29c8b6ab9438aca
Nick Chernyshev <nick.chernyshev@gmail.com>
2015-07-03 18:23:39 +0500

    fix assets compilation

--- config/initializers/slim.rb
+++ config/initializers/slim.rb
@@ -1,6 +1,6 @@
 Rails.application.assets.register_engine '.slim', Slim::Template
 
-Slim::Engine.set_options format: :html5
+Slim::Engine.set_options format: :html
 Slim::Engine.set_options attr_list_delims: {'(' => ')', '[' => ']'}
 Slim::Engine.set_options code_attr_delims: {'(' => ')', '[' => ']'}
 # Slim::Engine.set_options shortcut: {'.' => {:attr => 'class'}, '#' => {:attr => 'id'}, '@' => { attr: 'role' }}
